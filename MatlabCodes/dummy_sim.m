8c3fabdf3cf687a7c5a449a57288b22e7b845729
massimiliano.bertoni@studenti.unipd.it <massimiliano.bertoni@studenti.unipd.it>
2025-03-06 11:59:28 +0100

    feat: add computation of minimum distance from obstacles

--- MatlabCodes/dummy_sim.m
+++ MatlabCodes/dummy_sim.m
@@ -2,9 +2,9 @@ clear all;
 close all;
 
 plotting = false;
-alg_fmincon = 'sqp';
+alg_fmincon = 'active-set';
 fixed_horizon = true;
-sim_perception_range = false;
+sim_perception_range = true;
 sim_noise = false;
 sigma_2 = (0.15/3)^2;
 eps_loose_grip = 0.15;
@@ -19,7 +19,6 @@ fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d
          'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
         plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
         sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
-
 
 traj_fig_name = "trajectory.jpg";
 coord_fig_name = "coordinates.jpg";
