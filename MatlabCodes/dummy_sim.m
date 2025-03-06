3cd4b221d94b0aad6d3e7a22237ecab7511f6e7b
massimiliano.bertoni@studenti.unipd.it <massimiliano.bertoni@studenti.unipd.it>
2025-03-06 10:45:42 +0100

    feat: add a script for multiple consecutive simulations

--- MatlabCodes/dummy_sim.m
+++ MatlabCodes/dummy_sim.m
@@ -11,9 +11,15 @@ eps_loose_grip = 0.15;
 
 N_short = 5;
 N_long = 15;
-N_fixed = 15;
+N_fixed = 20;
 
-env_name = "two_obs";
+env_name = "valzer"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
 
 traj_fig_name = "trajectory.jpg";
 coord_fig_name = "coordinates.jpg";
---
+++ MatlabCodes/my_sim.m
@@ -0,0 +1,440 @@
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = true;
+sim_perception_range = false;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 20;
+
+env_name = "valzer"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%%%
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = false;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 20;
+
+env_name = "no_obs"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%%%
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = false;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 20;
+
+env_name = "two_obs"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%%%
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = false;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 20;
+
+env_name = "three_obs"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%%%
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = false;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 20;
+
+env_name = "valzer"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%%%
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = true;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 15;
+
+env_name = "no_obs"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%%%
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = true;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 15;
+
+env_name = "two_obs"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%%%
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = true;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 15;
+
+env_name = "three_obs"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%%%
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = true;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 15;
+
+env_name = "valzer"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%% 
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = true;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 5;
+
+env_name = "no_obs"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%% 
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = true;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 5;
+
+env_name = "two_obs"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%% 
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = true;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 5;
+
+env_name = "three_obs"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
+%%% 
+
+clear all;
+close all;
+
+plotting = false;
+alg_fmincon = 'active-set';
+fixed_horizon = true;
+sim_perception_range = true;
+sim_noise = false;
+sigma_2 = (0.15/3)^2;
+eps_loose_grip = 0.15;
+
+N_short = 5;
+N_long = 15;
+N_fixed = 5;
+
+env_name = "valzer"; % "no_obs", "two_obs", "three_obs", "valzer"
+
+fprintf(['plotting=%d, alg_fmincon=%s, fixed_horizon=%d, sim_perception_range=%d, sim_noise=%d, ' ...
+         'sigma_2=%.5f, eps_loose_grip=%.2f, N_short=%d, N_long=%d, N_fixed=%d, env_name=%s\n'], ...
+        plotting, alg_fmincon, fixed_horizon, sim_perception_range, sim_noise, ...
+        sigma_2, eps_loose_grip, N_short, N_long, N_fixed, env_name);
+
+
+traj_fig_name = "trajectory.jpg";
+coord_fig_name = "coordinates.jpg";
+vel_fig_name = "velocities.jpg";
+obj_dist_fig_name = "obstacle_distance.jpg";
+times_fig_name = "times.jpg";
+obs_fig_name = "obs.jpg";
+
+simCooperativeTransport;
+
