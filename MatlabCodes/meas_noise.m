3d743a8384f3c46bfafd00f23cfa119274a678a9
massimiliano.bertoni@studenti.unipd.it <massimiliano.bertoni@studenti.unipd.it>
2025-03-11 14:18:29 +0100

    feat: set up last mc analysis

--- MatlabCodes/meas_noise.m
+++ MatlabCodes/meas_noise.m
@@ -1,14 +1,11 @@
-function [noise] = meas_noise(sigma_2, sim_noise)
+function [noise] = meas_noise(sigma_2, sigma_2_v, sim_noise)
 %MEAS_NOISE function that computes a normal noise with covariance sigma_2
 %if sim_noise =1
 % noise only for x and y coordinates
     if sim_noise
-        mu = 0; % mean noise
-        R = chol(sigma_2);
-        noise_p = [mu + randn*R;
-            mu + randn*R];
-        noise = [noise_p;
-            zeros(2,1)]; % noise
+        R1 = chol(sigma_2);
+        R2 = chol(sigma_2_v);
+        noise = [randn*R1; randn*R1; randn*R1; randn*R2];
     else
         noise = 0;
     end
